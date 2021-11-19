47206aaa994e46046d78a580b9201af7975230c9
Kjell Maekelberg <67697869+KjellMeister-Severed@users.noreply.github.com>
2021-11-19 16:34:06 +0100

    test tailwind and fix broken package lock

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header flex flex-col-reverse justify-center">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to the MYT react app!
         </p>
         <a
           className="App-link"
