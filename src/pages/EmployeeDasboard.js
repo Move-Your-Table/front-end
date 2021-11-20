9db8cce613b77f80a3d705d69f6327ea1fb511ae
steffen.gemin <steffen.gemin@student.howest.be>
2021-11-20 20:51:54 +0100

    Add employee navbar

--- src/pages/EmployeeDasboard.js
+++ src/pages/EmployeeDasboard.js
@@ -1,8 +1,10 @@
 import React from "react";
+import EmployeeNavigation from "../components/Navigation/EmployeeNavigation";
 
 export default function EmployeeDasboard() {
   return (
-    <div>
+    <div className="container mx-auto px-4 ">
+      <EmployeeNavigation />
       <h1>Employee page</h1>
     </div>
   );
