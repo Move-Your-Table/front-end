152855bf0f7ffd43656026ec2ecc5873b4e4244f
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-12 16:41:16 +0100

    Format building data

--- src/components/Admin/TableComponent.js
+++ src/components/Admin/TableComponent.js
@@ -1,11 +1,11 @@
 import React from "react";
 
-export const TableComponent = ({ headers, data }) => {
+export const TableComponent = ({ headers, data, actions }) => {
   console.log(data);
   return (
     <section className="container mx-auto font-mono">
       <div className="w-full mb-8 overflow-hidden rounded-lg shadow-lg">
-        <div className="w-full overflow-x-auto">
+        <div className="w-full ">
           <table className="w-full">
             <thead>
               <tr className="text-md font-semibold tracking-wide text-left text-gray-900 bg-gray-100 uppercase border-b border-gray-600">
@@ -16,6 +16,11 @@ export const TableComponent = ({ headers, data }) => {
                     </th>
                   );
                 })}
+                {actions && (
+                  <th scope="col" key={"Actions"} className="px-4 py-3">
+                    Actions
+                  </th>
+                )}
               </tr>
             </thead>
             <tbody className="bg-white">
@@ -25,13 +30,22 @@ export const TableComponent = ({ headers, data }) => {
                     {Object.keys(item).map((key, index) => {
                       return (
                         <td
-                          key={index}
+                          key={`${item.id} ${index}`}
                           className="px-4 py-3 text-ms font-semibold border"
                         >
                           {item[key]}
                         </td>
                       );
                     })}
+                    {actions &&
+                      actions.map((action, index) => {
+                        <td
+                          key={`${item.id} ${index}`}
+                          className="px-4 py-3 text-ms font-semibold border"
+                        >
+                          action
+                        </td>;
+                      })}
                   </tr>
                 );
               })}
--- src/pages/Admin/BuildingsOverview.js
+++ src/pages/Admin/BuildingsOverview.js
@@ -1,29 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { TableComponent } from "../../components/Admin/TableComponent";
+import AdminService from "../../services/AdminService";
 
 const BuildingsOverview = () => {
+  const headers = [
+    "Number",
+    "Building Name",
+    "Total rooms",
+    "Total desks",
+    "Location"
+  ];
   const [buildingsJson, setbuildingsJson] = useState(null);
 
   useEffect(() => {
-    EmployeeService.getReservations().then((res) => {
-      setbuildingsJson(res);
-      console.log(buildingsJson);
+    AdminService.getBuildings().then((res) => {
+      setbuildingsJson(formatJson(res));
     });
   }, []);
+
+  function formatJson(res) {
+    let newArray = [];
+    res.map((item) => {
+      newArray.push({
+        id: item.id,
+        name: item.name,
+        totalRooms: item.rooms.total,
+        totalDesks: item.desks.total,
+        location: `${item.street}, ${item.postcode} ${item.city}, ${item.country}`
+      });
+    });
+    return newArray;
+  }
 
   return (
     <div className="container mx-auto px-4 ">
       <h1>Test</h1>
-      <TableComponent
-        headers={["Number", "Desk", "Name"]}
-        data={[
-          {
-            id: 0,
-            name: "Building Name 0",
-            street: "Street Name 0"
-          }
-        ]}
-      />
+      {buildingsJson !== null ? (
+        <TableComponent headers={headers} data={buildingsJson} />
+      ) : (
+        <h2>Loading..</h2>
+      )}
     </div>
   );
 };
