1d2e847d093dd1c354d19ec3534b8fb6aa293c67
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-18 11:48:31 +0100

    Clean employee service

--- src/services/EmployeeService.js
+++ src/services/EmployeeService.js
@@ -8,52 +8,40 @@ class EmployeeService {
   }
 
   async getDesks(buildingId, roomId) {
-    return fetch(
-      `${process.env.REACT_APP_API_URL}building/${buildingId}/room/${roomId}/desks`
-    )
-      .then((response) => response.json())
-      .then((data) => data);
+    return this.apiCall(`building/${buildingId}/room/${roomId}/desks`, "GET");
   }
 
   async getDesk(buildingId, roomId, deskId) {
-    return fetch(
-      `${process.env.REACT_APP_API_URL}building/${buildingId}/room/${roomId}/desks/${deskId}`
-    )
-      .then((response) => response.json())
-      .then((data) => data);
+    return this.apiCall(
+      `building/${buildingId}/room/${roomId}/desks/${deskId}`,
+      "GET"
+    );
   }
 
   async getReservations() {
-    return fetch(`${process.env.REACT_APP_API_URL}reservations?userId=1`) //Because we dont use authentication default user is 1
-      .then((response) => response.json())
-      .then((data) => data)
-      .catch((err) => console.log(err));
+    return this.apiCall(`reservations?userId=1`, "GET"); //Because we dont use authentication default user is 1
   }
 
   async makeNewReservation(buildingId, roomId, deskId, startTime, endTime) {
-    return fetch(`${process.env.REACT_APP_API_URL}reservations`, {
-      method: "POST",
-      headers: {
-        Accept: "application/json",
-        "Content-Type": "application/json"
-      },
-      body: JSON.stringify({
+    return this.apiCall(
+      `reservations`,
+      "POST",
+      JSON.stringify({
         buildingId: buildingId,
         roomId: roomId,
         deskId: deskId,
         startTime: startTime,
         endTime: endTime
       })
-    })
-      .then((response) => response.json())
-      .then((data) => data);
+    );
   }
 
   async apiCall(uri, httpVerb, requestBody) {
     const request = new Request(process.env.REACT_APP_API_URL + uri, {
       method: httpVerb,
       headers: {
-        "Content-type": "application/json;"
+        Accept: "application/json",
+        "Content-Type": "application/json"
       },
       body: JSON.stringify(requestBody)
     });
