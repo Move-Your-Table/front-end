48e757a49440b1c3f41117bcba227a4bcc5ac7c9
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-18 11:08:39 +0100

    Add modal

--- src/components/modals/Admin/NewDeskModal.js
+++ src/components/modals/Admin/NewDeskModal.js
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import SlideInModal from "../SlideInModal";
 
-const NewDeskModal = () => {
-  return <div>Test</div>;
+const NewDeskModal = ({ handleClose, isOpen }) => {
+  const [buildings, SetBuildings] = useState([]);
+  const [selectedBuilding, setSelectedBuilding] = useState(-1);
+  return (
+    <SlideInModal
+      handleClose={handleClose}
+      isOpen={isOpen}
+      title="New Desk"
+    ></SlideInModal>
+  );
 };
 
 export default NewDeskModal;
