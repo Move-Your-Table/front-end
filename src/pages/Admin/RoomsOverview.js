5f767ca2bc5002ad522e63d2bfb15e8b8584b001
steffen.gemin <steffen.gemin@student.howest.be>
2021-12-18 14:53:34 +0100

    Add overview to rooms

--- src/pages/Admin/RoomsOverview.js
+++ src/pages/Admin/RoomsOverview.js
@@ -1,9 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewRoomModal from "../../components/modals/Admin/NewRoomModal";
 import AdminNavigation from "../../components/Navigation/AdminNavigation";
+import SelectComponent from "../../components/Form/SelectComponent";
+import AdminService from "../../services/AdminService";
+import { TableComponent } from "../../components/Admin/TableComponent";
 
 const RoomsOverview = () => {
+  const headers = ["Name", "Type", "Capacity"];
   const [isCreateModalOpen, setCreateModalOpen] = useState(false);
+
+  const [buildings, SetBuildings] = useState([]);
+  const [selectedBuilding, setSelectedBuilding] = useState(-1);
+
+  const [searchString, setSearchString] = useState("");
+
+  const [roomsJson, setRoomsJson] = useState(null);
+
+  const formatJson = (res) => {
+    console.log(res);
+    let newArray = [];
+    res.map((item) => {
+      newArray.push({
+        name: item.roomName,
+        type: item.type,
+        capacity: item.capacity
+      });
+    });
+    return newArray;
+  };
+
+  const filterRooms = (item) => {
+    const itemName = item.name.toLowerCase();
+    return itemName.includes(searchString.toLowerCase());
+  };
+
+  useEffect(() => {
+    AdminService.getBuildings().then((res) => {
+      SetBuildings(res);
+    });
+  }, []);
+
+  useEffect(() => {
+    if (selectedBuilding !== -1) {
+      AdminService.getRooms(selectedBuilding)
+        .then((res) => setRoomsJson(res))
+        .catch(() => setRoomsJson([]));
+    }
+  }, [selectedBuilding]);
+
   return (
     <div className="container mx-auto px-4 ">
       <AdminNavigation />
@@ -20,22 +64,28 @@ const RoomsOverview = () => {
           </div>
         </div>
 
-        {/* {buildingsJson !== null ? (
+        <div className="flex text-white">
+          <SelectComponent
+            title="Building"
+            options={buildings}
+            selected={selectedBuilding}
+            setSelected={setSelectedBuilding}
+          />
+        </div>
+        {roomsJson !== null ? (
           <TableComponent
             headers={headers}
-            data={formatJson(buildingsJson).filter(filterBuilding)}
-            onDelete={(building) => {
-              AdminService.removeBuilding(building.id).then((r) =>
-                fetchBuildings()
-              );
+            data={formatJson(roomsJson)}
+            onDelete={() => {
+              console.log("Delete");
             }}
             onEdit={() => {
               console.log("Edit");
             }}
           />
         ) : (
-          <h2>Loading..</h2>
-        )} */}
+          <h2>Select building</h2>
+        )}
       </div>
 
       <NewRoomModal
